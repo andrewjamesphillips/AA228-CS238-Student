71a71787d51fb9e617b5e12362d7af397f9e9c9b
AJ Phillips <ajphillips@DNa80dc44.SUNet>
2023-02-02 15:51:34 -0800

    optimized bayesian score computation by component

--- project1/project1.py
+++ project1/project1.py
@@ -47,13 +47,18 @@ def compute(infile, outfile):
     bs = bayesian_score(vars, G, D)
     print(f"K2 search: {bs}")
 
+    # K2 search optimized
+    ordering = range(n)
+    G = K2_search_optimized(vars, D, r, ordering)
+    bs = bayesian_score(vars, G, D)
+    print(f"K2 search optimized: {bs}")
 
     # Oppurtunistic local search
-    Ginitial = nx.DiGraph()
-    Ginitial.add_nodes_from(range(n))
-    G = local_search(vars, D, Ginitial, 500)
-    bs = bayesian_score(vars, G, D)
-    print(f"Opportunistic local search: {bs}")
+    # Ginitial = nx.DiGraph()
+    # Ginitial.add_nodes_from(range(n))
+    # G = local_search(vars, D, Ginitial, 500)
+    # bs = bayesian_score(vars, G, D)
+    # print(f"Opportunistic local search: {bs}")
 
     # Plot G
     # labels = dict(zip(range(n),varnames))
@@ -110,13 +115,54 @@ def K2_search(vars, D, ordering):
                 break
     return G
 
+def K2_search_optimized(vars, D, r, ordering):
+    n = len(vars)
+    G = nx.DiGraph()
+    G.add_nodes_from(range(n))
 
-def bayesian_score(vars, G, D):
+    # Precomputations
+    component_scores = bayesian_score_optimized(vars, G, D)
+    parents = [np.array([j for j in G.predecessors(i)], dtype=int) for i in range(n)]
+
+    for (k,i) in enumerate(tqdm(ordering[1:])):
+        while True:
+            y_best, j_best = -np.inf, 0
+            for j in ordering[:k+1]:
+                if not G.has_edge(j,i):
+                    new_parents = np.concatenate(([j], parents[i]))
+                    y_prime = compute_graph_component(i, new_parents, r, D)
+                    if y_prime > y_best:
+                        y_best, j_best = y_prime, j
+            if y_best > component_scores[i]:
+                G.add_edge(j_best,i)
+                # Update bookkeeping
+                component_scores[i] = y_best
+                parents[i] = np.concatenate(([j_best], parents[i]))
+            else:
+                break
+    return G
+
+
+def compute_graph_component(i, new_parents, r, D):
+    m,n = D.shape
+    q = int(np.prod(r[new_parents]))
+    M = np.zeros((q, r[i]), dtype=int)
+    alpha = np.ones((q, r[i]), dtype=int)
+    for o in range(m):
+        k = D[o,i] - 1
+        j = 0
+        if new_parents.size > 0:
+            j = np.ravel_multi_index(D[o,new_parents] - 1, r[new_parents])
+        M[j,k] += 1
+    return bayesian_score_component(M,alpha)
+
+
+def bayesian_score_optimized(vars, G, D):
     n = len(vars)
     M = statistics(vars, G, D)
     alpha = prior(vars, G)
-    return sum(bayesian_score_component(M[i], alpha[i]) for i in range(n))
-
+    return np.array([bayesian_score_component(M[i], alpha[i]) for i in range(n)])
+    
 
 def bayesian_score_component(M, alpha):
     p = np.sum(sp.loggamma(alpha + M))
@@ -124,6 +170,13 @@ def bayesian_score_component(M, alpha):
     p += np.sum(sp.loggamma(np.sum(alpha, axis=1)))
     p -= np.sum(sp.loggamma(np.sum(alpha, axis=1) + np.sum(M, axis=1)))
     return p
+
+
+def bayesian_score(vars, G, D):
+    n = len(vars)
+    M = statistics(vars, G, D)
+    alpha = prior(vars, G)
+    return sum(bayesian_score_component(M[i], alpha[i]) for i in range(n))
 
 
 def statistics(vars, G, D):
@@ -134,7 +187,7 @@ def statistics(vars, G, D):
     for o in range(m):
         for i in range(n):
             k = D[o,i] - 1
-            parents = np.array([n for n in G.predecessors(i)])
+            parents = np.array([n for n in G.predecessors(i)], dtype=int)
             j = 0
             if parents.size > 0:
                 j = np.ravel_multi_index(D[o,parents] - 1, r[parents])
