6cce460f15faf25cacdeed718ca65466516c16cc
AJ Phillips <ajphillips@DNa80dc44.SUNet>
2023-02-01 18:23:31 -0800

    k2 search

--- project1/project1.py
+++ project1/project1.py
@@ -1,10 +1,12 @@
 import sys
 import networkx as nx
 import numpy as np
+import numpy.random as random
 import scipy.special as sp
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-DIR = "example"
+DIR = "data"
 
 class Variable:
     def __init__(self, name, r):
@@ -32,21 +34,68 @@ def compute(infile, outfile):
     r = np.max(D, axis=0)
     vars = np.array([Variable(varnames[i],r[i]) for i in range(n)])
 
-    # Build G
-    G = nx.DiGraph()
-    G.add_nodes_from(range(n))
-    edges = np.loadtxt(f"{DIR}/{outfile}", dtype=str, delimiter=',')
-    r,c = edges.shape
-    G.add_edges_from([tuple([np.where(edges[i,j]==varnames)[0][0] for j in range(c)]) for i in range(r)])
+    # Build G from example
+    # G = nx.DiGraph()
+    # G.add_nodes_from(range(n))
+    # edges = np.loadtxt(f"{DIR}/{outfile}", dtype=str, delimiter=',')
+    # r,c = edges.shape
+    # G.add_edges_from([tuple([np.where(edges[i,j]==varnames)[0][0] for j in range(c)]) for i in range(r)])
 
-    # Plot G
-    #labels = dict(zip(range(n),varnames))
-    #nx.draw_networkx(G, labels=labels, node_color='lightgray', arrowsize=20)
-    #plt.show()
+    # Directed graph search
+    ordering = range(n)
+    G = K2_search(vars, D, ordering)
 
     # Compute Bayesian score
     bs = bayesian_score(vars, G, D)
     print(bs)
+
+    # Plot G
+    labels = dict(zip(range(n),varnames))
+    pos = nx.planar_layout(G)
+    nx.draw_networkx(G, pos=pos, labels=labels, node_color='lightgray', arrowsize=20)
+    plt.show()
+
+
+# def rand_graph_neighbor(G):
+#     n = G.number_of_nodes()
+#     i = random.randint(0,n)
+#     j = (i + random.randint(1,n)) % n
+#     Gprime = G.copy()
+#     if G.has_edge(i,j):
+#         Gprime.remove_edge(i,j)
+#     else:
+#         Gprime.add_edge(i,j)
+#     return Gprime
+
+
+# def local_search(vars, D, G, k_max):
+#     y = bayesian_score(vars, G, D)
+#     for k in range(k_max):
+#         Gprime = rand_graph_neighbor(G)
+    
+
+def K2_search(vars, D, ordering):
+    n = len(vars)
+    G = nx.DiGraph()
+    G.add_nodes_from(range(n))
+    for (k,i) in enumerate(tqdm(ordering[1:])):
+        y = bayesian_score(vars, G, D)
+        while True:
+            y_best, j_best = -np.inf, 0
+            for j in ordering[:k+1]:
+                if not G.has_edge(j,i):
+                    G.add_edge(j,i)
+                    yprime = bayesian_score(vars, G, D)
+                    if yprime > y_best:
+                        y_best, j_best = yprime, j
+                    G.remove_edge(j,i)
+            if y_best > y:
+                y = y_best
+                G.add_edge(j_best,i)
+            else:
+                break
+    return G
+
 
 def bayesian_score(vars, G, D):
     n = len(vars)
@@ -54,12 +103,14 @@ def bayesian_score(vars, G, D):
     alpha = prior(vars, G)
     return sum(bayesian_score_component(M[i], alpha[i]) for i in range(n))
 
+
 def bayesian_score_component(M, alpha):
     p = np.sum(sp.loggamma(alpha + M))
     p -= np.sum(sp.loggamma(alpha))
     p += np.sum(sp.loggamma(np.sum(alpha, axis=1)))
     p -= np.sum(sp.loggamma(np.sum(alpha, axis=1) + np.sum(M, axis=1)))
     return p
+
 
 def statistics(vars, G, D):
     m,n = D.shape
@@ -76,11 +127,13 @@ def statistics(vars, G, D):
             M[i][j,k] += 1
     return M
 
+
 def prior(vars, G):
     n = len(vars)
     r = np.array([vars[i].r for i in range(n)])
     q = np.array([int(np.prod([r[j] for j in G.predecessors(i)])) for i in range(n)])
     return [np.ones((q[i], r[i]), dtype=int) for i in range(n)]
+
 
 def main():
     if len(sys.argv) != 3:
